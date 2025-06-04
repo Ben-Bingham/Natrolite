5a139536e4b1cf60731eaf8004199c267ee6348e
Ben-Bingham <ben@benbingham.ca>
2025-06-03 21:49:52 -0600

    Added basic preview

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,20 @@
 #include "Utilities/ImGui/ImGuiInstance.h"
 #include "Utilities/OpenGl/VertexAttributeObject.h"
 #include "Utilities/OpenGl/Buffer.h"
+#include "Utilities/OpenGl/Framebuffer.h"
+#include "Utilities/OpenGl/Renderbuffer.h"
 #include "Utilities/Camera.h"
-
-void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
-void MouseMovementCallback(GLFWwindow* window, double x, double y);
 
 Camera cam{ };
 
 std::shared_ptr<Window> window{ };
 
-int main() {
-    window = std::make_shared<Window>(glm::ivec2{ 1600, 1000 }, "Vanadium");
+glm::ivec2 imGuiWindowSize{ };
+glm::ivec2 lastImGuiWindowSize{ };
+glm::ivec2 viewPortOffset{ };
 
-    glfwSetFramebufferSizeCallback(window->handle, FramebufferSizeCallback);
-    glfwSetCursorPosCallback(window->handle, MouseMovementCallback);
-    glfwSetInputMode(window->handle, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+int main() {
+    window = std::make_shared<Window>(glm::ivec2{ 1600, 1000 }, "Natrolite");
 
     Context context{ *window };
    
@@ -36,7 +35,30 @@ int main() {
 
     Shader mainShader{ "assets\\shaders\\main.vert", "assets\\shaders\\main.frag" };
     mainShader.Bind();
-    mainShader.SetVec4("color", glm::vec4{0.0f, 1.0f, 0.0f, 1.0f});
+    mainShader.SetVec4("color", glm::vec4{ 0.0f, 1.0f, 0.0f, 1.0f });
+
+    Framebuffer framebuffer{ };
+
+    Texture2D texture{ window->size, TextureParameters{
+        TextureFormat::RGB,
+        TextureStorageType::UNSIGNED_BYTE,
+        TextureWrapMode::REPEAT,
+        TextureFilteringMode::LINEAR
+    } };
+
+    framebuffer.AddTexture(texture, Framebuffer::TextureUses::COLOR_0);
+
+    Renderbuffer renderbuffer{ window->size };
+
+    framebuffer.AddRenderbuffer(renderbuffer, Framebuffer::RenderbufferUses::DEPTH_STENCIL);
+
+    if (!framebuffer.Check()) {
+        std::cout << "ERROR, Framebuffer is not complete" << std::endl;
+    }
+
+    framebuffer.Unbind();
+    texture.Unbind();
+    renderbuffer.Unbind();
 
     std::vector<float> vertices{
         -0.5f, 0.0f, 0.0f,
@@ -57,7 +79,6 @@ int main() {
     glEnableVertexAttribArray(0);
 
     float lastFrame = 0.0f;
-
     while (!glfwWindowShouldClose(window->handle)) {
         float currentFrame = (float)glfwGetTime();
 
@@ -66,34 +87,42 @@ int main() {
 
         imGui.StartNewFrame();
         
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+
         // Show GUI
-        { ImGui::Begin("Viewport");
-        ImGui::Text("Hello World!");
+        { ImGui::Begin("Preview");
+            imGuiWindowSize = glm::ivec2{ ImGui::GetContentRegionAvail().x, ImGui::GetContentRegionAvail().y };
+
+            if (imGuiWindowSize != lastImGuiWindowSize) {
+                framebuffer.Bind();
+
+                texture.Bind();
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imGuiWindowSize.x, imGuiWindowSize.y, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.Get(), 0);
+
+                glViewport(0, 0, imGuiWindowSize.x, imGuiWindowSize.y);
+
+                renderbuffer.Bind();
+                glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, imGuiWindowSize.x, imGuiWindowSize.y);
+                glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderbuffer.Get());
+
+                framebuffer.Unbind();
+                texture.Unbind();
+                renderbuffer.Unbind();
+            }
+
+            viewPortOffset = glm::ivec2{ (int)ImGui::GetCursorPos().x, (int)ImGui::GetCursorPos().y };
+            ImGui::Image((ImTextureID)texture.Get(), ImVec2{ (float)imGuiWindowSize.x, (float)imGuiWindowSize.y });
+
         } ImGui::End();
 
         if (glfwGetKey(window->handle, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window->handle, true);
         }
 
-        // Camera Movement
-        if (glfwGetKey(window->handle, GLFW_KEY_W) == GLFW_PRESS) {
-            cam.position += cam.forward * cam.movementSpeed * dt;
-        }
-        if (glfwGetKey(window->handle, GLFW_KEY_S) == GLFW_PRESS) {
-            cam.position -= cam.forward * cam.movementSpeed * dt;
-        }
-        if (glfwGetKey(window->handle, GLFW_KEY_A) == GLFW_PRESS) {
-            cam.position -= cam.right * cam.movementSpeed * dt;
-        }
-        if (glfwGetKey(window->handle, GLFW_KEY_D) == GLFW_PRESS) {
-            cam.position += cam.right * cam.movementSpeed * dt;
-        }
-        if (glfwGetKey(window->handle, GLFW_KEY_SPACE) == GLFW_PRESS) {
-            cam.position += cam.up * cam.movementSpeed * dt;
-        }
-        if (glfwGetKey(window->handle, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-            cam.position -= cam.up * cam.movementSpeed * dt;
-        }
+        framebuffer.Bind();
 
         // Prep for rendering
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
@@ -101,15 +130,16 @@ int main() {
 
         glm::mat4 model{ 1.0f };
         glm::mat4 view = cam.ViewMatrix();
-        glm::mat4 projection = glm::perspective(glm::radians(60.0f), (float)window->size.x / (float)window->size.y, 0.01f, 100.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(60.0f), (float)imGuiWindowSize.x / (float)imGuiWindowSize.y, 0.01f, 100.0f);
 
         glm::mat4 mvp = projection * view * model;
 
         mainShader.Bind();
         mainShader.SetMat4("mvp", mvp);
 
-        // Render
         glDrawElements(GL_TRIANGLES, (unsigned int)indices.size(), GL_UNSIGNED_INT, nullptr);
+
+        framebuffer.Unbind();
 
         // Finish the GUI frame
         imGui.FinishFrame();
@@ -120,37 +150,4 @@ int main() {
     }
 
     imGui.Cleanup();
-}
-
-void FramebufferSizeCallback(GLFWwindow* w, int width, int height) {
-    glViewport(0, 0, width, height);
-    
-    window->size.x = width;
-    window->size.y = height;
-}
-
-void MouseMovementCallback(GLFWwindow* window, double x, double y) {
-    glm::vec2 pos{ (float)x, (float)y };
-
-    if (cam.lastMousePos.x == std::numeric_limits<float>::max()) {
-        cam.lastMousePos = pos;
-    }
-
-    glm::vec2 posOffset{ pos.x - cam.lastMousePos.x, cam.lastMousePos.y - pos.y };
-
-    cam.lastMousePos = pos;
-
-    posOffset *= cam.lookSensitivity;
-
-    cam.yaw += posOffset.x;
-    cam.pitch += posOffset.y;
-
-    cam.pitch = std::clamp(cam.pitch, -89.0f, 89.0f);
-
-    cam.forward.x = cos(glm::radians(cam.yaw)) * cos(glm::radians(cam.pitch));
-    cam.forward.y = sin(glm::radians(cam.pitch));
-    cam.forward.z = sin(glm::radians(cam.yaw)) * cos(glm::radians(cam.pitch));
-    cam.forward = glm::normalize(cam.forward);
-
-    cam.right = glm::normalize(glm::cross(cam.forward, cam.up));
 }
